cdfddeaf62838f8bd8044a86af2ed306bbfd57fc
andreitol <tolstoi.a@gmail.com>
2020-03-11 10:35:54 +0200

    feat: implement menu function - smooth scroll & active item of menu hightlite

--- script.js
+++ script.js
@@ -3,33 +3,48 @@ document.addEventListener('DOMContentLoaded', () => {
 
   //--- Smooth scrolling with web API: scrollIntoView --- //
   // const linkNav = document.querySelectorAll(`[href^='#']`);
-  // Menu items
+  // Get all Menu items with class="nav__item" inside nav block
   const linksNav = document.querySelectorAll(`.nav__item`);
-  console.log('linksNav', linksNav);
+  // Get all Menu target with id & section tag
+  const targetNav = document.querySelectorAll('section[id]');
+  console.log('targetNav', targetNav);
   const home = document.getElementById('home');
   const services = document.getElementById('services');
   const portfolio = document.getElementById('portfolio');
   const about = document.getElementById('about');
   const contact = document.getElementById('contact');
 
-  const handleButtonClick = target => {
-    console.log('HELLO', target);
+  /**
+   * Function handleLinkNavClick. Implement a smooth transition by clicking on a menu item to the target block
+   * @param {Element} target
+   */
+  const handleLinkNavClick = target => {
     target.scrollIntoView({
       block: 'start',
       behavior: 'smooth',
     });
   };
 
-  // --- Events --- //
-  // Services
-
-  linksNav[0].addEventListener('click', () => handleButtonClick(home));
-  linksNav[1].addEventListener('click', () => handleButtonClick(services));
-  linksNav[2].addEventListener('click', () => handleButtonClick(portfolio));
-  linksNav[3].addEventListener('click', () => handleButtonClick(about));
-  linksNav[4].addEventListener('click', () => handleButtonClick(contact));
+  for (let i = 0; i < linksNav.length; i++) {
+    linksNav[i].addEventListener('click', () => handleLinkNavClick(targetNav[i]));
+  }
 
   // --- Toggle menu --- //
-
   const nav = document.querySelector('.nav');
+  // Loop through the nav-items and add the active class to the current/clicked nav-items
+
+  linksNav.forEach(element => {
+    element.addEventListener('click', () => {
+      const current = document.querySelector('.nav__item_active');
+      const currentIsActive = current.classList.contains('nav__item_active');
+      const elementIsActive = element.classList.contains('nav__item_active');
+
+      if (currentIsActive === elementIsActive) {
+        return;
+      }
+
+      element.classList.add('nav__item_active');
+      current.classList.remove('nav__item_active');
+    });
+  });
 });
